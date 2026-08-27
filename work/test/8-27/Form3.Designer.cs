f89ae934ee79af680005b162ccaaf3bca65b4e73
lzd <3084506305@qq.com>
2026-08-27 19:56:00 +0800

    8-27及以前的练习

---
+++ work/test/8-27/Form3.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace WinFormsAppTest8_25
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tableLayoutPanel1 = new TableLayoutPanel();
+            button4 = new Button();
+            textBox2 = new TextBox();
+            button3 = new Button();
+            label2 = new Label();
+            LB_JD = new Label();
+            button2 = new Button();
+            label1 = new Label();
+            LB_XHS = new Label();
+            count = new Label();
+            price = new Label();
+            goodsNameLb = new Label();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            label4 = new Label();
+            tableLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 5;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 15F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 15F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 35F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 15F));
+            tableLayoutPanel1.Controls.Add(button4, 4, 2);
+            tableLayoutPanel1.Controls.Add(textBox2, 3, 2);
+            tableLayoutPanel1.Controls.Add(button3, 2, 2);
+            tableLayoutPanel1.Controls.Add(label2, 1, 2);
+            tableLayoutPanel1.Controls.Add(LB_JD, 0, 2);
+            tableLayoutPanel1.Controls.Add(button2, 4, 1);
+            tableLayoutPanel1.Controls.Add(label1, 1, 1);
+            tableLayoutPanel1.Controls.Add(LB_XHS, 0, 1);
+            tableLayoutPanel1.Controls.Add(count, 2, 0);
+            tableLayoutPanel1.Controls.Add(price, 1, 0);
+            tableLayoutPanel1.Controls.Add(goodsNameLb, 0, 0);
+            tableLayoutPanel1.Controls.Add(button1, 2, 1);
+            tableLayoutPanel1.Controls.Add(textBox1, 3, 1);
+            tableLayoutPanel1.Location = new Point(153, 84);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 3;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 33.3333321F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 33.3333321F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 33.3333321F));
+            tableLayoutPanel1.Size = new Size(479, 127);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // button4
+            // 
+            button4.Dock = DockStyle.Fill;
+            button4.Font = new Font("Microsoft YaHei UI", 15F, FontStyle.Regular, GraphicsUnit.Point, 134);
+            button4.Location = new Point(407, 87);
+            button4.Name = "button4";
+            button4.Size = new Size(69, 37);
+            button4.TabIndex = 13;
+            button4.Text = "+";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            textBox2.Dock = DockStyle.Fill;
+            textBox2.Location = new Point(240, 87);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(161, 37);
+            textBox2.TabIndex = 12;
+            textBox2.Text = "0";
+            textBox2.TextAlign = HorizontalAlignment.Center;
+            // 
+            // button3
+            // 
+            button3.Dock = DockStyle.Fill;
+            button3.Font = new Font("Microsoft YaHei UI", 15F, FontStyle.Regular, GraphicsUnit.Point, 134);
+            button3.Location = new Point(169, 87);
+            button3.Name = "button3";
+            button3.Size = new Size(65, 37);
+            button3.TabIndex = 11;
+            button3.Text = "-";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Dock = DockStyle.Fill;
+            label2.ForeColor = SystemColors.ControlText;
+            label2.Location = new Point(98, 84);
+            label2.Name = "label2";
+            label2.Size = new Size(65, 43);
+            label2.TabIndex = 10;
+            label2.Text = "5";
+            label2.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // LB_JD
+            // 
+            LB_JD.AutoSize = true;
+            LB_JD.Dock = DockStyle.Fill;
+            LB_JD.ForeColor = SystemColors.ControlText;
+            LB_JD.Location = new Point(3, 84);
+            LB_JD.Name = "LB_JD";
+            LB_JD.Size = new Size(89, 43);
+            LB_JD.TabIndex = 9;
+            LB_JD.Text = "鸡蛋";
+            LB_JD.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // button2
+            // 
+            button2.Dock = DockStyle.Fill;
+            button2.Font = new Font("Microsoft YaHei UI", 15F, FontStyle.Regular, GraphicsUnit.Point, 134);
+            button2.Location = new Point(407, 45);
+            button2.Name = "button2";
+            button2.Size = new Size(69, 36);
+            button2.TabIndex = 8;
+            button2.Text = "+";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Dock = DockStyle.Fill;
+            label1.ForeColor = SystemColors.ControlText;
+            label1.Location = new Point(98, 42);
+            label1.Name = "label1";
+            label1.Size = new Size(65, 42);
+            label1.TabIndex = 6;
+            label1.Text = "3";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // LB_XHS
+            // 
+            LB_XHS.AutoSize = true;
+            LB_XHS.Dock = DockStyle.Fill;
+            LB_XHS.ForeColor = SystemColors.ControlText;
+            LB_XHS.Location = new Point(3, 42);
+            LB_XHS.Name = "LB_XHS";
+            LB_XHS.Size = new Size(89, 42);
+            LB_XHS.TabIndex = 3;
+            LB_XHS.Text = "西红柿";
+            LB_XHS.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // count
+            // 
+            count.AutoSize = true;
+            tableLayoutPanel1.SetColumnSpan(count, 3);
+            count.Dock = DockStyle.Fill;
+            count.ForeColor = SystemColors.ControlText;
+            count.Location = new Point(169, 0);
+            count.Name = "count";
+            count.Size = new Size(307, 42);
+            count.TabIndex = 2;
+            count.Text = "数量";
+            count.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // price
+            // 
+            price.AutoSize = true;
+            price.Dock = DockStyle.Fill;
+            price.ForeColor = SystemColors.ControlText;
+            price.Location = new Point(98, 0);
+            price.Name = "price";
+            price.Size = new Size(65, 42);
+            price.TabIndex = 1;
+            price.Text = "单价";
+            price.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // goodsNameLb
+            // 
+            goodsNameLb.AutoSize = true;
+            goodsNameLb.Dock = DockStyle.Fill;
+            goodsNameLb.ForeColor = SystemColors.ControlText;
+            goodsNameLb.Location = new Point(3, 0);
+            goodsNameLb.Name = "goodsNameLb";
+            goodsNameLb.Size = new Size(89, 42);
+            goodsNameLb.TabIndex = 0;
+            goodsNameLb.Text = "商品名称";
+            goodsNameLb.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            button1.Dock = DockStyle.Fill;
+            button1.Font = new Font("Microsoft YaHei UI", 15F, FontStyle.Regular, GraphicsUnit.Point, 134);
+            button1.Location = new Point(169, 45);
+            button1.Name = "button1";
+            button1.Size = new Size(65, 36);
+            button1.TabIndex = 5;
+            button1.Text = "-";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Dock = DockStyle.Fill;
+            textBox1.Location = new Point(240, 45);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(161, 36);
+            textBox1.TabIndex = 7;
+            textBox1.Text = "0";
+            textBox1.TextAlign = HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(153, 366);
+            label3.Name = "label3";
+            label3.Size = new Size(54, 20);
+            label3.TabIndex = 1;
+            label3.Text = "总计：";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(212, 366);
+            label4.Name = "label4";
+            label4.Size = new Size(18, 20);
+            label4.TabIndex = 2;
+            label4.Text = "0";
+            // 
+            // Form3
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(tableLayoutPanel1);
+            Name = "Form3";
+            Text = "Form3";
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private Label goodsNameLb;
+        private Label label1;
+        private Label LB_XHS;
+        private Label count;
+        private Label price;
+        private Button button1;
+        private TextBox textBox1;
+        private Button button4;
+        private TextBox textBox2;
+        private Button button3;
+        private Label label2;
+        private Label LB_JD;
+        private Button button2;
+        private Label label3;
+        private Label label4;
+    }
+}
---
+++ work/test/8-27/Form3.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace WinFormsAppTest8_25
+{
+    public partial class Form3 : Form
+    {
+        
+        List<Dictionary<string,Control>> ctrlList = new List<Dictionary<string,Control>>();
+        public Form3()
+        {
+            InitializeComponent();
+
+            InitFrom();
+        }
+
+        //先将每行的数据组成一个字典或类放到list里
+        //输入框事件触发取出所对应的单价 -相乘
+        //遍历-赋值返回
+
+        private void InitFrom()
+        {
+            ctrlList.Add(new Dictionary<string, Control>
+            {
+                ["price"] = label1,
+                ["count"] = textBox1,
+                ["sub"] = button1,
+                ["add"] = button2
+
+            });
+
+            ctrlList.Add(new Dictionary<string, Control>
+            {
+                ["price"] = label2,
+                ["count"] = textBox2,
+                ["sub"] = button3,
+                ["add"] = button4
+            });
+            //给每个输入框添加事件
+            ctrlList.ForEach(ctrl => {
+                ctrl["count"].TextChanged += TextBox_TextChanged;
+                ctrl["sub"].Click += SubBtn_Click;
+                ctrl["add"].Click += AddBtn_Click;
+            });
+
+            
+
+        }
+
+        private void AddBtn_Click(object? sender, EventArgs e)
+        {
+            var dic = ctrlList.Find(item => item["add"] == sender as Button);
+            dic["count"].Text = (int.Parse(dic["count"].Text) +1).ToString();
+
+        }
+
+        private void SubBtn_Click(object? sender, EventArgs e)
+        {
+            //ctrlList.ForEach(item => { item["count"].Text = (int.Parse(item["count"].Text) - 1).ToString(); });
+            var dic=ctrlList.Find(item =>  item["sub"] == sender as Button);
+            dic["count"].Text=(int.Parse(dic["count"].Text)-1).ToString();
+
+        }
+
+        private void TextBox_TextChanged(object sender, EventArgs e)
+        {
+            int sum = 0;
+            ctrlList.ForEach(item =>
+            {
+                if (item["count"].Text.Length>0)
+                {
+                    if (!Regex.IsMatch(item["count"].Text,@"^[0-9]*$"))
+                    {
+                        item["count"].Text = "0";
+                        (item["count"] as TextBox).SelectionStart = 1;
+                    }
+                    sum += int.Parse(item["count"].Text) * int.Parse(item["price"].Text);
+
+                }
+                
+
+            });
+            label4.Text = sum.ToString();
+        }
+    }
+}
