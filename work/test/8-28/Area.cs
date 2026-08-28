b08c5f4b4fff0632cdb1b2a681e783840dafcdf3
lzd <3084506305@qq.com>
2026-08-28 20:23:25 +0800

    8-28的作业

---
+++ work/test/8-28/Area.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WinFormsAppTest8_25
+{
+    public class Area
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int FatherId { get; set; }
+        public Area(int Id, string Name, int FatherId)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.FatherId = FatherId;
+        }
+
+    }
+}
